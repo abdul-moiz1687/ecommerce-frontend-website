6ecae2646cb50db4a0436f79493727db5712bf2f
abdul-moiz1687 <abdulmoizarain766@gmail.com>
2026-08-31 22:42:12 +0500

    fixsignuplink

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,10 +1,11 @@
 import "./TopBar.css";
+import { Link } from "react-router-dom";
 
 function TopBar() {
   return (
     <div className="top-bar">
       <p>  Sign up and get 20% off to your first order.{" "}
-        <a href="/Signup">Sign Up Now</a>
+       <Link to="/signup">Sign Up Now</Link>
       </p>
 
       <button className="top-bar-close" aria-label="Close"> ×</button>

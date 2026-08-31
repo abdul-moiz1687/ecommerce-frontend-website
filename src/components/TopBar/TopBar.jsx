8a6427b0749f2f9d2b86ee09b4a76744eafa38c6
abdul-moiz1687 <abdulmoizarain766@gmail.com>
2026-08-31 22:33:41 +0500

    correctsignup path

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -4,7 +4,7 @@ function TopBar() {
   return (
     <div className="top-bar">
       <p>  Sign up and get 20% off to your first order.{" "}
-        <a href="/signup">Sign Up Now</a>
+        <a href="/Signup">Sign Up Now</a>
       </p>
 
       <button className="top-bar-close" aria-label="Close"> ×</button>
